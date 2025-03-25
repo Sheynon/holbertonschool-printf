0d44bc1d0f70672446d1b4b4c0b7fccea1b13c49
C-Florian <florianchereau@hotmail.fr>
2025-03-25 23:52:35 +0100

    added-prototypes function to the header file

--- test/main.h
+++ test/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
 
-#endif
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(char c);
+
+#endif /* MAIN_H */
