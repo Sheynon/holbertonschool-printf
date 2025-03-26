7d1e48301978b2c7881f10f55abe3702fb0eb057
romain344 <romainleoulzec@gmail.com>
2025-03-26 14:03:10 +0100

    Fix print_percent function to return % instead of using -putchar('%')

--- logan/conversion.c
+++ logan/conversion.c
@@ -1,33 +1,11 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - Function that outputs a formatted string,
- *           handling the %% specifier.
- * @format: The format string containing the specifiers.
- * 
- * Return: The number of characters printed.
+ * print_percent - Function that prints the '%' character.
+ *
+ * Return: Always returns % (success).
  */
-int _printf(const char *format, ...)
+int print_percent(void)
 {
-	int count = 0; // iinialise count
-	const char *ptr; // ptr is the character string
-
-	for (ptr = format; *ptr != '\0'; ptr++) // initializes the character string
-	{
-			if (*ptr == '%' && *(ptr + 1) == '%') //displays character 1 if % or 2 if %%
-			{
-
-					write(1, "%", 1); //discplay the characters
-					count++;
-					ptr++;
-			}
-			else
-			{
-					write(1, ptr, 1); //displays a normal character
-
-					count++;
-			}
-	}
-			return (count); //returns the value
+	return (-putchar('%'));
 }
