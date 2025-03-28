cae92a3551f1eeaeb2e24685386540ab268670c2
C-Florian <florianchereau@hotmail.fr>
2025-03-28 14:50:40 +0100

    added - _printf file

--- test/_printf.c
+++ test/_printf.c
@@ -1,34 +1,44 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
 
 /**
- * _printf - Produces output according to a format (handles %c).
- * @format: The format string containing directives.
- * Return: The number of characters printed (excluding null byte).
+ * _printf - Produces output according to a format
+ * @format: Format string containing conversion specifiers
+ *
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
+	int i = 0, count = 0;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	while (*format)
+	while (format[i])
 	{
-		if (*format == '%' && (*(format + 1) == 'c'))
+		if (format[i] == '%' && format[i + 1])
 		{
-			format++;
-			count += print_char(va_arg(args, int));
+			i++;
+			if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
+				count += print_string(va_arg(args, char *));
+			else if (format[i] == '%')
+				count += _putchar('%');
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_int(va_arg(args, int));
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
 		}
 		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
+			count += _putchar(format[i]);
+		i++;
 	}
 
 	va_end(args);
